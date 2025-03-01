5b23c28c7fcd1d2baa44cf365ab0fba6408974a4
Forhad Reza <forhad.bimt@gmail.com>
2025-03-02 00:37:53 +0600

    Some beautiful animation and shadow added

--- scripts/bg-change.js
+++ scripts/bg-change.js
@@ -5,18 +5,18 @@ function getRandomColor() {
     color += letters[Math.floor(Math.random() * 16)];
   }
   // set opacity
-  // color += "30";
+  color += "30";
   return color;
 }
 
 document.getElementById("theme-btn").addEventListener("click", (event) => {
   let randomColor = getRandomColor();
-  // document.body.style.backgroundColor = randomColor;
-  document.querySelectorAll(".bg-blueExtraLight").forEach((element) => {
-    if (element.tagName === "BODY") {
-      element.style.backgroundColor = randomColor.concat("35");
-    } else {
-      element.style.backgroundColor = randomColor.concat("10");
-    }
-  });
+  document.body.style.backgroundColor = randomColor;
+  // document.querySelectorAll(".bg-blueExtraLight").forEach((element) => {
+  //   if (element.tagName === "BODY") {
+  //     element.style.backgroundColor = randomColor.concat("35");
+  //   } else {
+  //     element.style.backgroundColor = randomColor.concat("10");
+  //   }
+  // });
 });
